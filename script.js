7008851b811029d327f6cd823834a74e265e1136
Socialite1 <tgsekgololo@gmail.com>
2026-03-08 15:10:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ const supabaseUrl = "https://cqutkhetpnylhconaodf.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxdXRraGV0cG55bGhjb25hb2RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NTE1MjEsImV4cCI6MjA4ODUyNzUyMX0.CegeRNrSg7vtVCBhn8vsINbebBmoqWzktu3jVB4hbCg"
 
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey)
+
+async function saveLocation(name, link){
+
+const { data, error } = await supabaseClient
+.from("locations")
+.insert([
+{
+name: name,
+qr_link: link,
+scan_count: 0,
+points: 0
+}
+])
+
+if(error){
+console.log("Error saving location:", error)
+}
+else{
+console.log("Location saved:", data)
+}
+
+}
