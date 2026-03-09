009027e681606faad62945cdef79185658d900e1
Socialite1 <tgsekgololo@gmail.com>
2026-03-09 14:28:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
+if ("serviceWorker" in navigator) {
+
+navigator.serviceWorker.register("service-worker.js")
+.then(() => {
+console.log("Service Worker Registered")
+})
+
+}
 const supabaseUrl = "https://cqutkhetpnylhconaodf.supabase.co"
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxdXRraGV0cG55bGhjb25hb2RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5NTE1MjEsImV4cCI6MjA4ODUyNzUyMX0.CegeRNrSg7vtVCBhn8vsINbebBmoqWzktu3jVB4hbCg"
 
